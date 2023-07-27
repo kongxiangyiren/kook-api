fb99e5176f64a1b2689b5ee761753485735123d8
空巷一人 <1530688385@qq.com>
2023-07-27 15:15:25 +0800

    fix

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import 'isomorphic-fetch';
 import Bot from './lib/bot';
 
 const KBot = new Bot();
--- src/lib/bot.ts
+++ src/lib/bot.ts
@@ -3,6 +3,7 @@ import check from './check';
 import KBot from '../KBot';
 import { createWs } from './createws';
 import { downloadFfmpeg } from './tools/ffmpeg';
+import { execSync } from 'child_process';
 
 export default class Bot {
   async run() {
@@ -17,6 +18,11 @@ export default class Bot {
       console.log('ffmpeg下载失败');
     } else {
       console.log('ffmpeg下载成功');
+      
+      if (process.platform === 'linux') {
+        console.log('添加运行权限');
+        execSync('chmod +x ' + ffmpegPath);
+      }
       KBot.ffmpegPath = ffmpegPath;
     }
 
